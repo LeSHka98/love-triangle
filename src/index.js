9efab68e94a803e8f473c05e101b03d6e3fd2b99
Alexei Petrovskiy <pas1-by@yandex.ru>
2019-03-17 16:55:45 +0300

    add love-triangle task solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   let  n = 0, a=[];
+   outer:
+   for (let i = 0, b = preferences.length; i < b; i++) {
+       for (let q = 0; q < a.length; q++) {
+           if (i + 1 == a[q]) {
+               continue outer;
+           }
+       }
+       if (preferences[i] != i+1) 
+       {
+          let j = preferences[i] - 1;
+          let k = preferences[j] - 1;
+          if (preferences[k] == i + 1) {
+              n++;
+
+              a.push(preferences[i]);
+              a.push(preferences[j]);
+              a.push(preferences[k]);
+          }   
+      }
+  }
+  return n;
 };
